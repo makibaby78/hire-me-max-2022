bb5151df5a88c3f3fae688ca88850c10350c53ec
Macky Baby <raindrops.dark@gmail.com>
2022-11-04 23:06:08 +0800

    Contact

--- src/Screens/Contacts/Contacts.js
+++ src/Screens/Contacts/Contacts.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FaLockOpen, FaLock, FaEnvelope } from 'react-icons/fa';
+import { FaLockOpen, FaLock, FaEnvelope, FaHouseUser, FaPhoneAlt, FaGlobeAsia } from 'react-icons/fa';
 import './Contacts.css';
 
 function Contacts() {
@@ -10,7 +10,7 @@ function Contacts() {
     };
 
   return (
-    <div className={`resume-wrapper cs ${hide}`}>
+    <div className={`contact-wrapper resume-wrapper cs ${hide}`}>
         <div className={`bi-it-cs bi-it-con ${hide}`} >
             <button className={`bi-icon-cs bi-icon-con ${hide}`} onClick={()=>themeToggler()}>
                 <FaEnvelope />
@@ -23,10 +23,60 @@ function Contacts() {
             </button>
         </div>
         <div className={`contact-content ${hide}`}>
-            Test
+            <iframe title='google-map'
+              src="https://maps.google.com/maps?q=consolacion%20tayud&t=&z=13&ie=UTF8&iwloc=&output=embed"
+              width="100%"
+              height="350"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            />
+            <div className='ci-c-wrapper'>
+                    <div className='ci-wrapper'>
+                        <div>
+                            <div className='ci-c-header'>
+                                <h3>SEND ME A MESSAGE</h3>
+                            </div>
+                            <div className='ne-wrapper'>
+                                <div className='li-wrapper'>
+                                    <label className='fc-4' type='text'>Name</label>
+                                    <input type="text" name='name' />
+                                </div>
+                                <div className='li-wrapper'>
+                                    <label className='fc-4' type='text'>Email</label>
+                                    <input type="email" name='email' />
+                                </div>
+                            </div>
+                            <div className='li-wrapper'>
+                                <label className='fc-4' type='text'>Your Message</label>
+                                <textarea type="long-text" />
+                            </div>
+                            <button className='btn-ci'>Submit</button>
+                        </div>
+                        <div className='ci-card'>
+                            <h4>CONTACT INFO</h4>
+                            <div className='icon-details'>
+                                <FaHouseUser /> <span className='fc-1'>East Binabag Tayud, Consolacion Cebu</span>
+                            </div>
+                            <div className='icon-details'>
+                                <FaPhoneAlt /> <span className='fc-1'>09453529874</span>
+                            </div>
+                            <div className='icon-details'>
+                                <FaHouseUser /> <span className='fc-1'>raindrops.dark@gmail.com</span>
+                            </div>
+                            <div className='icon-details'>
+                                <FaGlobeAsia /> <span className='fc-1'>https://makibaby78.github.io/hire-me-max-2022/</span>
+                            </div>
+                        </div>
+                        
+                </div>
+            </div>
         </div>
     </div>
   )
 }
+
 
 export default Contacts   
